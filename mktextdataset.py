b205204cfb1a6c9787ecb40bf2ec5cb5b36876ea
zmfkzj <qlwlal@naver.com>
2020-03-30 14:07:42 +0900

    util.chgext()의 인수 path가 경로거나 경로를 포함한 iterable 객체 모두 작동하도록 변경, 그 외 버그 수정

--- mktextdataset.py
+++ mktextdataset.py
@@ -3,35 +3,38 @@ from sklearn.model_selection import train_test_split
 import glob
 import shutil
 from util import *
-from toolz.curried import *
 from toolz import curry
 
 def readListFromFolder(path, root):
     imglist = []
-    for (p, d, f) in os.walk(os.path,join(root, path)):
-        imglist_ = filter(lambda i: not (i.endswith(".png")|i.endswith(".jpg")), f)
-        imglist_ = filteredimglist(imglist_, p)
-        imglist_ = list(map(lambda file: os.path.relpath(os.path.join(p, file), root), f))
+    for (p, d, f) in os.walk(path):
+        imglist_ = filter(lambda i: (i.endswith(".png")|i.endswith(".jpg")), f)
+        # imglist_ = filteredimglist(imglist_, p)
+        imglist_ = list(map(lambda file: os.path.relpath(os.path.join(p, file), root), imglist_))
         print(f'{p} 폴더에 {len(imglist_)}장의 이미지가 있습니다.')
         imglist.extend(imglist_)
     return imglist
 
 def existAnno(path, root):
-    txt = os.path.isfile(os.path.join(root,chgext(path, '.txt')[0]))
-    xml = os.path.isfile(os.path.join(root,chgext(path, '.xml')[0]))
+    txt = os.path.isfile(os.path.join(root,chgext(path, '.txt')))
+    xml = os.path.isfile(os.path.join(root,chgext(path, '.xml')))
     return (xml | txt)
 
-filteredimglist = lambda List, root: list(filter(lambda elm: not existAnno(elm, root), List))
+filteredimglist = lambda List, root: list(filter(lambda elm: existAnno(1, root), List))
 
 def readListFromtxt(path, root):
     imglist = txt2list(path)
     imglist = filteredimglist(imglist, root)
     return imglist
 
+def chgelm(List, idx, newvalue):
+    List[idx]=newvalue
+    return List
+
 def chgMidFd(pathlist, midname):
     allsplitlist = [path.split('/') for path in pathlist]
     # chgsplitlist = [splitpath[1]=midname for splitpath in allsplitlist]
-    chgsplitlist = list(map(lambda splitpath: splitpath[1] = midname, allsplitlist))
+    chgsplitlist = list(map(lambda splitpath: os.path.join(*chgelm(splitpath,1,midname)), allsplitlist))
     return chgsplitlist
 
 
@@ -49,22 +52,22 @@ def mkhardlink(src, link_dir):
 def mkTextDataset(path, test_size=0.2):
     imglist = []
     if os.path.isdir(path):
-        print('#1')
         root = os.path.normpath(f'{path}/..')
         imglist = readListFromFolder(path,root)
     elif os.path.isfile(path):
-        print('#2')
+        if os.path.basename(path) in ['train.txt', 'test.txt']:
+            print('파일명을 train.txt, test.txt가 아닌 것으로 바꿔주세요.')
+            return
         root = os.path.normpath(f'{os.path.split(path)[0]}/..')
-        print(path, root)
         imglist = readListFromtxt(path,root)
     else:
         print('파일 경로를 확인하세요')
         return
     print(f'총 {len(imglist)}장의 이미지가 있습니다.')
     
-    train, test = train_test_split(imglist, test_size = test_size, random_state=1)
-    train = chgMidFd(train, 'train')
-    test = chgMidFd(test, 'test')
+    trainOrigin, testOrigin = train_test_split(imglist, test_size = test_size, random_state=1)
+    trainChgMidFd = chgMidFd(trainOrigin, 'train')
+    testChgMidFd = chgMidFd(testOrigin, 'test')
 
     trainDatasetPath = os.path.join(root, f'data/train.txt')
     testDatasetPath = os.path.join(root, f'data/test.txt')
@@ -74,27 +77,27 @@ def mkTextDataset(path, test_size=0.2):
     DatasetBundle = [trainDatasetPath, testDatasetPath, allDatasetPath]
     list2txt(DatasetBundle, DatasetBundleSavePath)
 
-    list2txt(train, trainDatasetPath)
-    print(f'{len(train)}장의 train data list를 만들었습니다.')
-    trainXmlList = chgext(train, '.xml')
-    trainTxtList = chgext(train, '.txt')
-    for img in train:
-        mkhardlink(os.path.join(root, img), os.path.join(root,'data/tain'))
+    list2txt(trainChgMidFd, trainDatasetPath)
+    print(f'{len(trainChgMidFd)}장의 train data list를 만들었습니다.')
+    trainXmlList = chgext(trainOrigin, '.xml')
+    trainTxtList = chgext(trainOrigin, '.txt')
+    for img in trainOrigin:
+        mkhardlink(os.path.join(root, img), os.path.join(root,'data/train'))
     for xml in trainXmlList:
         xmlwithroot = os.path.join(root, xml)
         if os.path.isfile(xmlwithroot):
-            mkhardlink(xmlwithroot, os.path.join(root,'data/tain'))
+            mkhardlink(xmlwithroot, os.path.join(root,'data/train'))
     for txt in trainTxtList:
         txtwithroot = os.path.join(root, txt)
         if os.path.isfile(txtwithroot):
-            mkhardlink(txtwithroot, os.path.join(root,'data/tain'))
+            mkhardlink(txtwithroot, os.path.join(root,'data/train'))
 
 
-    list2txt(test, testDatasetPath)
-    print(f'{len(test)}장의 test data list를 만들었습니다.')
-    testXmlList = chgext(test, '.xml')
-    testTxtList = chgext(test, '.txt')
-    for img in test:
+    list2txt(testChgMidFd, testDatasetPath)
+    print(f'{len(testChgMidFd)}장의 test data list를 만들었습니다.')
+    testXmlList = chgext(testOrigin, '.xml')
+    testTxtList = chgext(testOrigin, '.txt')
+    for img in testOrigin:
         mkhardlink(os.path.join(root, img), os.path.join(root, 'data/test'))
     for xml in testXmlList:
         xmlwithroot = os.path.join(root, xml)
@@ -105,11 +108,11 @@ def mkTextDataset(path, test_size=0.2):
         if os.path.isfile(txtwithroot):
             mkhardlink(txtwithroot, os.path.join(root, 'data/test'))
 
-    list2txt(train+test, allDatasetPath)
+    list2txt(trainChgMidFd+testChgMidFd, allDatasetPath)
     print('모든 이미지 경로를 all.txt에 저장했습니다.')
 
 
 
 if __name__ == "__main__":
     # mkTextDataset('/home/tm/Code/darknet/data/train.txt')
-    mkTextDataset('/home/tm/data/train.txt')
+    mkTextDataset('/home/tm/Code/darknet/data/original.txt',test_size=0.3)
--- util.py
+++ util.py
@@ -1,4 +1,6 @@
 import os
+from collections.abc import Iterable
+from toolz import curry
 
 def txt2list(txtpath):
     #텍스트 파일의 내용을 리스트로 만들기
@@ -8,16 +10,22 @@ def txt2list(txtpath):
     filelist = [path for path in filelist if path]
     return filelist
 
-def chgext(pathlist, newext):
+def chgext(path, newext):
+    ispath = curry(isinstance)(path)
+    assert (ispath(str) | ispath(Iterable)), 'path is \'str\' or \'iterable\' type'
     '''
-    리스트 각 원소의 확장자 변경
+    path의 확장자 변경
     ex) newext = '.xml'
     '''
-    newpathlist = []
-    for path in pathlist:
+    def sub(path, newext):
         other, _ = os.path.splitext(path)
-        newpathlist.append(f'{other}{newext}')
-    return newpathlist
+        newpath = f'{other}{newext}'
+        return newpath
+    
+    if isinstance(path,str):
+        return sub(path, newext)
+    elif isinstance(path, Iterable):
+        return [sub(p,newext) for p in path]
 
 def list2txt(List, savepath):
     with open(savepath,'w') as f:
