9f5ecca1e49ac690847755590a276fc239cc319f
Chanho Nam <skacksgh1@gmail.com>
2021-10-14 19:01:33 +0900

    apms module editing...

--- 일회용/APMS/image_filter_and_move.py
+++ 일회용/APMS/image_filter_and_move.py
@@ -1,12 +1,22 @@
 from exif import Image
 from pathlib import Path
-from numpy.lib.type_check import real
 import pandas as pd
 from pyproj import Transformer
 import numpy as np
 import os
 from shutil import copy
+import psycopg2 as ps
+import argparse
 
+def parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input_dir", type=str, default="d:/upload_images/")
+    parser.add_argument("--output_dir", type=str, default="d:/moved_images")
+    parser.add_argument("--sensor_size", type=tuple, default=(8.8,13.2),help='h,w')
+    parser.add_argument("--focal_length", type=float, default=8.8)
+    parser.add_argument("--distance", type=float, default=50000)
+    parser.add_argument("--north_angle", type=float, default=78.17,help='이미지 12시 기준으로 실제 북쪽이 몇도에 있는지')
+    return parser.parse_args()
 
 def get_distance(tm1,tm2):
     return np.sqrt(np.sum(np.square(np.array(tm1)-np.array(tm2))))
@@ -21,6 +31,12 @@ def get_tm_coord(latitude,longitude):
     single_number_gps_info = np.sum(gps_info / np.array((1,60,3600)),axis=1)
 
     transformer = Transformer.from_crs('epsg:4737','epsg:5186')
+    return transformer.transform(*single_number_gps_info)
+
+def get_gps_coord(latitude,longitude):
+    single_number_gps_info = np.array((latitude,longitude))
+
+    transformer = Transformer.from_crs('epsg:5186','epsg:4737')
     return transformer.transform(*single_number_gps_info)
 
 def cal_FOV(sensor_size, focal_length, distance):
@@ -56,28 +72,49 @@ def cvt_polar2cartesian(polar_field):
     y = np.sin(angle)*distance
     return np.concatenate([np.expand_dims(y,2),np.expand_dims(x,2)],axis=2)
 
-dir_path = Path()
-db_gps_info = pd.DataFrame(columns='section sample slab gps_lt gps_rb'.split())
+def isin_image(img_ltrb, db_ltrb):
+    img_lt, img_rb = img_ltrb
+    db_lt, db_rb = db_ltrb
+    db_lt = get_tm_coord(*db_lt)
+    db_rb = get_tm_coord(*db_rb)
+
+    if np.all(img_lt<=db_lt) and np.all(img_rb>=db_rb):
+        return True
+    else:
+        return False
+
+arg = parser()
+
+db = []
+with ps.connect(host='localhost', dbname='aroad', user='postgres', password='namchanho1!@',port=5432) as conn:  # db에 접속
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM gps_test")
+        for data in cur:
+            db.append(pd.Series(data))
+
+db_gps_info = pd.DataFrame(db).rename(columns={0:'lt_lat',1:'lt_lon',2:'rb_lat',3:'rb_lon',4:'slab_id'})
 
 images = []
-for root,dirs,files in os.walk(dir_path):
+for root,dirs,files in os.walk(arg.input_dir):
     for f in files:
         if Path(f).suffix.lower() in ['.jpg','.png']:
             images.append(Path(root)/f)
 
 for img in images:
     img_tm = get_image_gps_info(img)
-    real_size = cal_FOV()
-    polar_field = get_polar_field((1080,1620),(60,160))
-    rotated_polar_field = rotate(polar_field,79.83)
-    cartesian_field = cvt_polar2cartesian(rotated_polar_field)
-    lt = cartesian_field[0,0,:]) 
-    rb = cartesian_field[-1,-1,:]
-    db_gps_info['new_image'] = db_gps_info['gps_center'].map(lambda db_tm: img if get_distance(db_tm,img_tm)<1 else np.nan)
+    real_size = cal_FOV(arg.sensor_size, arg.focal_length,arg.distance)
 
-for slab in db_gps_info.itertuples:
-    if slab.new_image is not None:
-        new_path = Path(slab.section)/slab.sample/slab.new_image.name
-        copy(str(slab.new_image),str(new_path))
+    with open(img,'r+b') as f:
+        img = Image(f)
     
-if __name__=="__main__":
+    polar_field = get_polar_field((img.pixel_y_dimension,img.pixel_x_dimension),real_size)
+    rotated_polar_field = rotate(polar_field,arg.north_angle)
+    cartesian_field = cvt_polar2cartesian(rotated_polar_field)
+    lt = cartesian_field[0,0,:]*real_size+img_tm
+    rb = cartesian_field[-1,-1,:]*real_size+img_tm
+
+    for slab in db_gps_info['lt_lat lt_lon rb_lat rb_lon'.split()].itertuples():
+        if isin_image((lt,rb),((slab.lt_lat,slab.lt_lon),(slab.rb_lat,slab.rb_lon))):
+            new_path = Path(arg.output_dir)/Path(img).name
+            os.makedirs(str(new_path.parent),exist_ok=True)
+            copy(str(img),str(new_path))
